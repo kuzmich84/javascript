ba4c44b52374efa1ab21b8fdd92efa4030898b40
Dmitry Kuchukov <kudim@inbox.ru>
2019-03-03 16:46:05 +0300

    button games

---
+++ index.js
@@ -0,0 +1,25 @@
+var $start=document.querySelector("#start");
+var $game=document.querySelector("#game");
+
+$start.addEventListener("click",startGame);
+
+function startGame() {
+    $game.style.backgroundColor="#fff";
+    $start.classList.add("hide");
+
+    renderBox(0);
+}
+
+function renderBox(){
+    var box=document.createElement("div");
+    box.style.height=box.style.width="50px";
+    box.style.position="absolute";
+    box.style.backgroundColor="#000";
+    box.style.top="50px";
+    box.style.left="70px";
+    box.style.cursor="pointer";
+
+    $game.insertAdjacentElement("afterbegin",box);
+
+
+}
